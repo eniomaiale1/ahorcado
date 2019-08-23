6dc9b3a76f1534047abcb921d420628f8c7f315c
enio.maiale <enio.maiale@heat.com>
2019-08-23 10:26:04 -0500

    New definition steps

--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -3,9 +3,13 @@ Given(/^que abri la aplicacion$/) do
     visit '/'
   end
   
-  Then(/^debo ver "([^"]*)"$/) do |titulo|
+  When(/^inicio el juego$/) do
+  end
 
+  Then(/^debo ver un titulo de bienvenida "([^"]*)"$/) do |arg1|
+    pending # Write code here that turns the phrase above into concrete actions
   end
   
-  When(/^inicio el juego$/) do
+  Then(/^debo ver un titulo de juego iniciado "([^"]*)"$/) do |arg1|
+    pending # Write code here that turns the phrase above into concrete actions
   end
